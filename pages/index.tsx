a7e0c696b34dc083ac1b1568575e9a1d8bce10c4
Leonardo Felipe <tleonardo186@gmail.com>
2021-08-28 00:58:03 -0300

    Upload files

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao";
+import Link from "next/link";
+import EntradaNumerica from "../components/EntradaNumerica";
+import { useState } from "react";
+
+export default function Formulario() {
+
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
+
+  return (
+    <div className={styles.formulario}>
+
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao>
+          <EntradaNumerica text={"Quantidade de Portas"} value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)}/>
+        </Cartao>
+      </div>
+    
+      <div>
+        <Cartao>
+        <EntradaNumerica text={"Porta com Presente"} value={comPresente} onChange={novaPortacomPresente => setComPresente(novaPortacomPresente)}/>
+        </Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link passHref href={`/jogo/${qtdePortas}/${comPresente}`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+
+    </div>
+  )
+}
---
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,58 @@
+import styles from '../../../styles/Jogo.module.css'
+import { useEffect, useState } from "react"
+import Porta from "../../../components/Porta"
+import criarPortas, { atualizarPortas } from "../../../functions/portas"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+export default function Jogo() {
+    const router = useRouter()
+
+    const [valido, setValido] = useState(false)
+    const [portas, setPortas] = useState([])
+
+    useEffect(() => {
+      const portas = +router.query.portas
+      const temPresente = +router.query.temPresente
+
+      const qtdePortasValidas =  portas >=3 && portas <= 30
+      const temPresenteValido = temPresente >= 1 && temPresente <= portas
+      
+      setValido(qtdePortasValidas && temPresenteValido)
+      setPortas(criarPortas(portas, temPresente))
+    }, [portas, router.query.portas, router.query.temPresente])
+
+    useEffect(() => {
+      const portas = +router.query.portas
+      const temPresente = +router.query.temPresente
+      
+      setPortas(criarPortas(portas, temPresente))
+    }, [router?.query])
+
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))} />
+       
+    })
+  }
+
+  return (
+    <div id={styles.jogo}>
+        <div className={styles.portas}>
+          {valido ? 
+            renderizarPortas() : 
+            <h1>Valores Invalidos</h1>
+            }
+        </div>
+
+        <div className={styles.botoes}>
+            <Link passHref href="/">
+                <button>Reiniciar Jogo</button>
+            </Link>
+        </div>
+
+    </div>
+  )
+
+}
